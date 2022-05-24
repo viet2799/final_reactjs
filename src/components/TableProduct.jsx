20b5093038218bc7a1624fea711b76c1aa39320f
viet2799 <nguyenducviet2799@gmail.com>
2022-05-24 14:43:27 +0700

    add disable

--- src/components/TableProduct.jsx
+++ src/components/TableProduct.jsx
@@ -1,9 +1,10 @@
-import React, {useState, useEffect} from "react";
-import {Table} from 'reactstrap'
-import {Checkbox, Button, Modal, Input, Pagination, Skeleton} from 'antd'
+import React, { useState, useEffect } from "react";
+import { Table } from 'reactstrap'
+import { Checkbox, Button, Modal, Input, Pagination, Skeleton, Alert } from 'antd'
 import "antd/dist/antd.css";
-import {DeleteOutlined, FormOutlined} from "@ant-design/icons";
+import { DeleteOutlined, FormOutlined } from "@ant-design/icons";
 import axios from "axios";
+import { useForm } from "react-hook-form";
 
 function TableProduct(props) {
     const URL_POST = '/api/tda/ims-be/product';
@@ -19,6 +20,13 @@ function TableProduct(props) {
     // setPage setPagesize
     const [page, setPage] = useState(1);
     const [pageSize, setPageSize] = useState(10);
+
+    //value input
+    const [value, setValue] = useState();
+
+    //disable btn 
+    const [disable, setDisable] = useState(false)
+    const [noti, setNoti] = useState()
 
     // loading
     const addOk = () => {
@@ -52,22 +60,21 @@ function TableProduct(props) {
 
     return (
 
-        <div style={{marginTop: '50px'}}>
+        <div style={{ marginTop: '50px' }}>
             {/* <Skeleton avatar paragraph={{ rows: 15 }}  /> */}
-            <div style={{padding: '0 0 40px 0', justifyContent: 'flex-start', display: 'flex'}}>
+            <div style={{ padding: '0 0 40px 0', justifyContent: 'flex-start', display: 'flex' }}>
                 <Button type="primary" onClick={() => {
                     setIsModalAdd(true)
                 }}>Add Product</Button>
             </div>
             {
-                (props.loading) ? <Skeleton paragraph={{rows: 12}}/> :
-                    (<Table>
-                        <thead>
+                <Table>
+                    <thead>
                         <tr>
                             <th>
                                 Id
                             </th>
-                            <th style={{textAlign: 'left'}}>
+                            <th style={{ textAlign: 'left' }}>
                                 Name
                             </th>
                             <th>
@@ -80,36 +87,63 @@ function TableProduct(props) {
                                 Edit
                             </th>
                         </tr>
-                        </thead>
-                        <tbody>
-                        {props.data?.map((item, key) => {
-                            return (
-                                <tr key={key}>
-                                    <th scope="row">
-                                        {item?.id}
-                                    </th>
-                                    <td style={{textAlign: 'left'}}>
-                                        {item?.name}
-                                    </td>
-                                    <td>
-                                        <Checkbox checked={item?.status} disabled/>
-                                    </td>
-                                    <td>
-                                        <DeleteOutlined style={{color: 'red'}}/>
-                                    </td>
-                                    <td>
-                                        <FormOutlined style={{color: '#40a9ff'}}
-                                                      onClick={() => {
-                                                          setIsModalEdit(true);
-                                                          setId(item.id);
-                                                          setName(item.name);
-                                                      }}/>
-                                    </td>
-                                </tr>
-                            )
-                        })}
-                        </tbody>
-                    </Table>)
+                    </thead>
+                    {
+                        (props.loading) ?
+                            (<tbody>
+                                {props.data?.map((item, key) => {
+                                    return (
+                                        <tr key={key}>
+                                            <th scope="row">
+                                                <Skeleton.Input />
+                                            </th>
+                                            <td style={{ textAlign: 'left' }}>
+                                                <Skeleton.Input />
+                                            </td>
+                                            <td>
+                                                <Skeleton.Input />
+                                            </td>
+                                            <td>
+                                                <Skeleton.Input />
+                                            </td>
+                                            <td>
+                                                <Skeleton.Input />
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>)
+                            :
+                            (<tbody>
+                                {props.data?.map((item, key) => {
+                                    return (
+                                        <tr key={key}>
+                                            <th scope="row">
+                                                {item?.id}
+                                            </th>
+                                            <td style={{ textAlign: 'left' }}>
+                                                {item?.name}
+                                            </td>
+                                            <td>
+                                                <Checkbox checked={item?.status} disabled />
+                                            </td>
+                                            <td>
+                                                <DeleteOutlined style={{ color: 'red' }} />
+                                            </td>
+                                            <td>
+                                                <FormOutlined style={{ color: '#40a9ff' }}
+                                                    onClick={() => {
+                                                        setIsModalEdit(true);
+                                                        setId(item.id);
+                                                        setName(item.name);
+                                                    }} />
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>)
+                    }
+                </Table>
             }
 
             <Pagination
@@ -117,7 +151,7 @@ function TableProduct(props) {
                 showTotal={total => `Total ${props.total} items`}
                 defaultPageSize={pageSize}
                 defaultCurrent={page}
-                style={{marginTop: '50px'}}
+                style={{ marginTop: '50px' }}
                 onChange={(page, pageSize) => {
                     setPage(page);
                     setPageSize(pageSize);
@@ -130,52 +164,64 @@ function TableProduct(props) {
 
             {/* Modal add */}
             <Modal title="Add Product" visible={isModalAdd}
-                   onOk={() => {
-                       addOk();
-                       setIsModalAdd(false);
-                   }}
-                   onCancel={() => {
-                       setIsModalAdd(false);
-                   }}>
-                <Input.Group style={{display: 'flex'}}>
-                    <label style={{width: '30%'}}>Name : </label>
+                onOk={() => {
+                    addOk();
+                    setIsModalAdd(false);
+                }}
+                onCancel={() => {
+                    setIsModalAdd(false);
+                }}
+                okButtonProps={(value == "") && { disabled: () => { setDisable(true) } }}
+            >
+                <Input.Group style={{ display: 'flex' }}>
+                    <label style={{ width: '30%' }}>Name : </label>
                     <Input placeholder="Add name , please"
-                           allowClear
-                           tyle={{width: '65%'}}
-                           value={name}
-                           onChange={(e) => setName(e.target.value)}
-
+                        allowClear
+                        tyle={{ width: '65%' }}
+                        value={name}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                            setValue(e.target.value)
+                        }}
                     />
+
                 </Input.Group>
-                <Input.Group style={{display: 'flex', marginTop: '20px'}}>
-                    <label style={{width: '22.6%'}}>Status : </label>
+                {(value === "") && (<p style={{ color: 'red', marginLeft: '108px' }}>Bạn cần phải điểm tên vào</p>)}
+                <Input.Group style={{ display: 'flex', marginTop: '20px' }}>
+                    <label style={{ width: '22.6%' }}>Status : </label>
                     <Checkbox
-                        tyle={{width: '75%'}}
-                        onChange={(e) => setStatus(!status)}/>
+                        tyle={{ width: '75%' }}
+                        onChange={(e) => setStatus(!status)} />
                 </Input.Group>
             </Modal>
             {/* Modal edit */}
             <Modal title="Product detail" visible={isModalEdit}
-                   onOk={() => {
-                       editOk(selectId);
-                       setIsModalEdit(false);
-                   }}
-                   onCancel={() => {
-                       setIsModalEdit(false)
-                   }}
+                onOk={() => {
+                    editOk(selectId);
+                    setIsModalEdit(false);
+                }}
+                onCancel={() => {
+                    setIsModalEdit(false)
+                }}
+
+                okButtonProps={(value === "") ? { disabled: () => { setDisable(true) } } : { disabled: { disable } }}
             >
-                <Input.Group style={{display: 'flex'}}>
-                    <label style={{width: '30%'}}>Name : </label>
+                <Input.Group style={{ display: 'flex' }}>
+                    <label style={{ width: '30%' }}>Name : </label>
                     <Input
                         placeholder="Add name , please"
-                        allowClear tyle={{width: '65%'}}
+                        allowClear tyle={{ width: '65%' }}
                         value={name}
-                        onChange={(e) => setName(e.target.value)}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                            setValue(e.target.value)
+                        }}
                     />
                 </Input.Group>
-                <Input.Group style={{display: 'flex', marginTop: '20px'}}>
-                    <label style={{width: '22.6%'}}>Status : </label>
-                    <Checkbox tyle={{width: '75%', marginLeft: '30px'}} disabled/>
+                {(value === "") && (<p style={{ color: 'red', marginLeft: '108px' }}>Bạn cần phải điểm tên vào</p>)}
+                <Input.Group style={{ display: 'flex', marginTop: '20px' }}>
+                    <label style={{ width: '22.6%' }}>Status : </label>
+                    <Checkbox tyle={{ width: '75%', marginLeft: '30px' }} disabled />
                 </Input.Group>
             </Modal>
         </div>
